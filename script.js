102cbc6aa6f7fa3fdf5e66add3164549e3502182
TyWalter <walterty89@gmail.com>
2024-04-23 00:39:35 -0500

    Nearly done with JS coding

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const button = document.querySelector('form');
 let inputClear = document.getElementById('city');
 
+// function clear(){
+//   document.getElementById('zero').innerHTML = "";
+//   document.getElementById('one').innerHTML = "";
+//   document.getElementById('two').innerHTML = "";
+//   document.getElementById('three').innerHTML = "";
+//   document.getElementById('four').innerHTML = "";
+// }
+
 function paramSet(){
   const city = localStorage.getItem('CitySearch');
   convertCity(city);
@@ -48,7 +56,7 @@ function grabApiCurrent(lat, lon){
 };
 
 function grabApiWeek(lat, lon){
-  const week = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=c1dd9688874f70a75ae85555d4ee0cd2`
+  const week = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=33&units=imperial&appid=c1dd9688874f70a75ae85555d4ee0cd2`
   fetch(week)
   .then(function (resp){
     return resp.json();
@@ -64,70 +72,53 @@ function grabApiWeek(lat, lon){
 
 function getWeatherCurrent(weather){
   const cDate = weather.dt;
-  const cIcon = weather.weather.icon;
+  const cIcon = weather.weather[0].icon;
   const cTemp = weather.main.temp;
   const cWind = weather.wind.speed;
   const cHumidity = weather.main.humidity;
-  // printCurrent(cDate, cIcon, cTemp, cWind, cHumidity);
+  printCurrent(cDate, cIcon, cTemp, cWind, cHumidity);
 };
 
 function getWeatherWeek(weather){
   for(let i=0; i<5; i++){
-    let wDate = weather.list[i].dt_txt;
-    const wIcon = weather.list[i].weather.icon;
-    const wTemp = weather.list[i].main.temp;
-    const wWind = weather.list[i].wind.speed;
-    const wHumidity = weather.list[i].main.humidity;
-    
+    const wDate = weather.list[i*8].dt_txt;
+    const wIcon = weather.list[i*8].weather[0].icon;
+    const wTemp = weather.list[i*8].main.temp;
+    const wWind = weather.list[i*8].wind.speed;
+    const wHumidity = weather.list[i*8].main.humidity;
     printWeek(wDate, wIcon, wTemp, wWind, wHumidity, i);
   };
 };
 
-// function printCurrent(date, icon, temp, wind, humidity){
-//   const cityName = document.createElement('h3');
-//   cityName.textContent = print.name;
-//   const cityTime = document.createElement('span');
-//   cityTime.textContent = print.dt;
-//   const cityIcon = document.createElement('span');
-//   cityIcon.textContent = print.weather.icon;
-//   const cityTemp = document.createElement('p');
-//   cityTemp.textContent = print.main.temp;
-//   const cityWind = document.createElement('p');
-//   cityWind.textContent = print.wind.speed;
-//   const cityHumidity = document.createElement('p');
-//   cityHumidity.textContent = print.main.humidity;
+function printCurrent(date, icon, temp, wind, humidity){
+  const day = dayjs(date);
+  const dateCurrent = document.querySelector('h3');
+  const iconCurrent = document.querySelector('span');
+  const tempCurrent = document.querySelector('.temp');
+  const windCurrent = document.querySelector('.wind');
+  const humidityCurrent = document.querySelector('humidity');
 
-//   const card = document.createElement('div');
-//   card.setAttribute('style', 'line-height: .5rem; padding: 5px; text-shadow: black 0 0 .3rem;')
-//   card.appendChild(nameEl) 
-//   card.appendChild(addressEl)
-//   card.appendChild(postalEl)
-//   card.appendChild(phoneEl)
+  tempCurrent.setAttribute
 
-//   const currentWeather = document.querySelector(`.card${i}`);
-//   if (currentWeather) {
-//     currentWeather.prepend(card);
-//   }
-// }
+}
 
 function printWeek(date, icon, temp, wind, humidity, i){
-  const forecastWeather = document.querySelector(`.data-5day-weather${i}`)
+  const day = dayjs(date);
+  const forecastWeather = document.querySelector(`.data-5day-weather${i}`);
   const card = document.querySelector(`.card${i}`);
-  const weekForecast = document.querySelector('.data-5day-forecast')
-  const forecastDate = document.querySelector('h5')
-  const iconForecast = document.querySelector('.data-5day-icon')
-  const temperature = document.querySelector('.temp');
-  const windSpeed = document.querySelector('.wind')
-  const humidityPercentage = document.querySelector('.humidity')
-  
-  forecastDate.textContent = date;
-  iconForecast.textContent = icon;
-  temperature.textContent = temp;
-  windSpeed.textContent = wind;
-  humidityPercentage.textContent = humidity;
-  console.log(temperature)
-  
+  const weekForecast = document.querySelector('.data-5day-forecast');
+  const forecastDate = document.querySelector('h5');
+  const iconForecast = document.querySelector(`.data-5day-icon${i}`);
+  const temperature = document.querySelector(`.temp${i}`);
+  const windSpeed = document.querySelector(`.wind${i}`);
+  const humidityPercentage = document.querySelector(`.humidity${i}`);
 
+  iconForecast.setAttribute('src', `http://openweathermap.org/img/wn/${icon}.png`);
+  forecastDate.textContent = day.format('MM/DD/YYYY');
+  temperature.textContent = `Temp: ${temp}\u00B0F`;
+  windSpeed.textContent = `Wind: ${wind} MPH`;
+  humidityPercentage.textContent = `Humidity: ${humidity} %`;
+  
   forecastWeather.appendChild(temperature);
   forecastWeather.appendChild(windSpeed);
   forecastWeather.appendChild(humidityPercentage);
@@ -136,31 +127,6 @@ function printWeek(date, icon, temp, wind, humidity, i){
   card.appendChild(forecastWeather);
   weekForecast.appendChild(card);
 };
-
-  // const cityName = document.createElement('h3');
-  // cityName.textContent = print.name;
-  // const cityTime = document.createElement('span');
-  // cityTime.textContent = print.dt;
-  // const cityIcon = document.createElement('span');
-  // cityIcon.textContent = print.weather.icon;
-  // const cityTemp = document.createElement('p');
-  // cityTemp.textContent = print.main.temp;
-  // const cityWind = document.createElement('p');
-  // cityWind.textContent = print.wind.speed;
-  // const cityHumidity = document.createElement('p');
-  // cityHumidity.textContent = print.main.humidity;
-
-  // const card = document.createElement('div');
-  // card.setAttribute('style', 'line-height: .5rem; padding: 5px; text-shadow: black 0 0 .3rem;')
-  // card.appendChild(nameEl) 
-  // card.appendChild(addressEl)
-  // card.appendChild(postalEl)
-  // card.appendChild(phoneEl)
-
-  // const currentWeather = document.querySelector(`.card${i}`);
-  // if (currentWeather) {
-  //   currentWeather.prepend(card);
-  // }
 
 function saveToStorage(e){
   e.preventDefault();
